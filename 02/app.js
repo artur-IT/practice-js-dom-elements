464ba34b2ee9f86ed7e3d6674ebf285bfcf4862e
Artur Matysiak <inz.matys@gmail.com>
2023-12-09 19:22:20 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
-console.log('DOM');
+const element = document.querySelectorAll("[data-url]");
+const link = document.querySelector("a");
+
+element.forEach((item, idx) => {
+  console.log(item.dataset.url, idx);
+  item.href = item.dataset.url;
+});
