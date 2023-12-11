fe909839bc2396235a8113b891b54fd1bb744287
Artur Matysiak <inz.matys@gmail.com>
2023-12-11 14:21:06 +0100

    task done

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,29 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+const newUl = document.createElement("ul");
+document.querySelector("nav").appendChild(newUl);
+// FIRST solution
+// newUl.innerHTML = `
+// <li>
+//   <a href="/">start</a>
+// </li>
+// <li>
+//   <a href="/gallery">galeria</a>
+// </li>
+// <li>
+//   <a href="/contact">kontakt</a>
+// </li>`;
+
+// SECOND solution
+menuItems.forEach((item) => {
+  const li = document.createElement("li");
+  newUl.appendChild(li);
+  li.innerHTML = `<a href="${item.url}">${item.text}</a>`;
+});
